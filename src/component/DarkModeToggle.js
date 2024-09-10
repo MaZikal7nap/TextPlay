71d5dcbe7a54083c47e88c2ddd9ad4536a3f1e43
MaZiK <nikhilbisht707@gmail.com>
2024-09-10 16:45:13 +0530

    Update DarkModeToggle.js

--- src/component/DarkModeToggle.js
+++ src/component/DarkModeToggle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './DarkModeToggle.css'; // Import CSS for styling
+import './DarkModeToggle.css'; 
 
 const DarkModeToggle = ({ mode, toggleMode }) => {
     return (
